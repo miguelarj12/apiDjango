93e7ecb41bb90e4d1dd634fe8661cef23bb3ecc0
miguelarj12 <miguelaguilas12@hotmail.com>
2023-09-21 19:17:45 -0600

    corregido bien

--- API9ISC22/API9ISC22/urls.py
+++ API9ISC22/API9ISC22/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from api.views import Home
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='index'),
 ]
--- API9ISC22/api/views.py
+++ API9ISC22/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
+class Home(APIView):
+    template_name = 'index.html'
+    def get(self, request):
+        return render(request, self.template_name)
